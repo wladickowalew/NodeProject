3fd813f512197623bedd7fa1af3615f3e6626b70
Wlad Kowalew <wladickowalew@gmail.com>
2019-08-12 13:17:49 +0300

    End commit

--- server/index.js
+++ server/index.js
@@ -7,4 +7,9 @@ app.get("/",function (req, res) {
 	res.send("Hello, Node.js!");
 });
 
+app.get("/folder/page", function(req, res){
+	let x = req.query.foo;
+	res.send({foo: x+x});
+});
+
 app.listen(591);
